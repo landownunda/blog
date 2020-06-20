562fc0813fd88ffbed172fc15d035c29aed32076
landownunda <routingwithjon@gmail.com>
2020-06-20 19:59:36 +1000

    Additional Commit with demo1 js function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
   }
+function myFunction1() {
+  document.getElementById("demo1").innerHTML = "Paragraph changed again.";
+  }
